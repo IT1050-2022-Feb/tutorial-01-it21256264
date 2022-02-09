da7f1250881d1fdbc130625947d9e39f45ce14a4
it21256264 <99328215+it21256264@users.noreply.github.com>
2022-02-09 19:39:03 +0530

    Update Tute01.c
    get  2 inputs for subjects marks by using scanf and calculate the average and using printf displays the average

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,17 @@
 #include <stdio.h>
 
 int main() {
+   int mark1,mark2;//declere the variable
+   float avg;
+   
+   printf("enter the mark 1 :");//getting user input
+   scanf("%d",&mark1);
+   printf("enter the mark 2 :");
+   scanf("%d",&mark2);
+   
+   avg = (mark1 + mark2)/2.0;//calculate the average
+   
+   printf("average :%.1f",avg);//display the average
   
   return 0;
 }
